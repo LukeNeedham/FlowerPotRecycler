146c4146d4d83c1d5ef153b6728abb9165e69c5a
Luke <49884591+LukeNeedham@users.noreply.github.com>
2019-12-05 13:51:10 +0100

    Update CyclicPositionDelegate.kt

--- flowerpotrecycler/src/main/java/com/lukeneedham/flowerpotrecycler/simpleadapter/positiondelegate/CyclicPositionDelegate.kt
+++ flowerpotrecycler/src/main/java/com/lukeneedham/flowerpotrecycler/simpleadapter/positiondelegate/CyclicPositionDelegate.kt
@@ -4,15 +4,25 @@ class CyclicPositionDelegate<ItemType>(private val items: List<ItemType>) :
     AdapterPositionDelegate<ItemType> {
 
     override fun getItemAt(position: Int): ItemType {
-        val cyclicPosition = position % items.size
-        return items[cyclicPosition]
+        val positionInCycle = position % items.size
+        return items[positionInCycle]
     }
 
-    override fun getItemCount() =
-        ITEM_COUNT
+    override fun getItemCount() = items.size * NUMBER_OF_CYCLES
+        
+    override fun getPositionOfItem(item: ItemType): Int {
+        val centerCycleIndex = NUMBER_OF_CYCLES / 2
+        val startPositionOfCenterCycle = centerCycleIndex * items.size
+        
+        val positionOfItemInCycle = items.indexOf(item)
+        if(positionOfItemInCycle == -1) {
+            throw Exception("Item $item does not exist in $items")
+        }
+        return startPositionOfCenterCycle + positionOfItemInCycle
+    }
 
     companion object {
         // Using Int.MAX_VALUE can cause OOMs. This value should be large enough that the user never reaches the end, but small enough to be memory efficient
-        const val ITEM_COUNT = 10000
+        const val NUMBER_OF_CYCLES = 1000
     }
 }
