0a4434cd92cc46b122596ca1ba1c240d99e9ab18
Luke <49884591+LukeNeedham@users.noreply.github.com>
2019-12-05 13:51:34 +0100

    Update CyclicPositionDelegate.kt

--- flowerpotrecycler/src/main/java/com/lukeneedham/flowerpotrecycler/simpleadapter/positiondelegate/CyclicPositionDelegate.kt
+++ flowerpotrecycler/src/main/java/com/lukeneedham/flowerpotrecycler/simpleadapter/positiondelegate/CyclicPositionDelegate.kt
@@ -23,6 +23,6 @@ class CyclicPositionDelegate<ItemType>(private val items: List<ItemType>) :
 
     companion object {
         // Using Int.MAX_VALUE can cause OOMs. This value should be large enough that the user never reaches the end, but small enough to be memory efficient
-        const val NUMBER_OF_CYCLES = 1000
+        private const val NUMBER_OF_CYCLES = 1000
     }
 }
