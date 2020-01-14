91641cbe9a6f7282d2057f5a1ad03f4b2dcb054b
Luke <49884591+LukeNeedham@users.noreply.github.com>
2020-01-14 17:19:46 +0100

    Update LinearPositionDelegate.kt

--- flowerpotrecycler/src/main/java/com/lukeneedham/flowerpotrecycler/simpleadapter/positiondelegate/LinearPositionDelegate.kt
+++ flowerpotrecycler/src/main/java/com/lukeneedham/flowerpotrecycler/simpleadapter/positiondelegate/LinearPositionDelegate.kt
@@ -1,10 +1,21 @@
 package com.lukeneedham.flowerpotrecycler.simpleadapter.positiondelegate
 
-class LinearPositionDelegate<ItemType>(private var items: List<ItemType> = emptyList()) :
+class LinearPositionDelegate<ItemType>(
+    adapter: RecyclerView.Adapter<*>,
+    diffCallback: DiffUtil.ItemCallback<ItemType>
+):
     AdapterPositionDelegate<ItemType> {
         
+    private val asyncListDiffer = AsyncListDiffer<ItemType>(
+        AdapterListUpdateCallback(adapter),
+        AsyncDifferConfig.Builder<ItemType>(diffCallback).build()
+    )
+        
+    private val items: List<ItemType>
+        get() = asyncListDiffer.currentList
+        
     override fun submitList(list: List<ItemType>) {
-        this.items = items
+        asyncListDiffer.submitList(list)
     }
         
     override fun getItems(): List<ItemType> = items
