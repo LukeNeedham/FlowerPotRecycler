4c34e71747d3bc6899d9bcc95de7c56532a54cc5
Luke <49884591+LukeNeedham@users.noreply.github.com>
2020-01-14 15:50:28 +0100

    Update AdapterPositionDelegate.kt

--- flowerpotrecycler/src/main/java/com/lukeneedham/flowerpotrecycler/simpleadapter/positiondelegate/AdapterPositionDelegate.kt
+++ flowerpotrecycler/src/main/java/com/lukeneedham/flowerpotrecycler/simpleadapter/positiondelegate/AdapterPositionDelegate.kt
@@ -2,6 +2,7 @@ package com.lukeneedham.flowerpotrecycler.simpleadapter.positiondelegate
 
 interface AdapterPositionDelegate<ItemType> {
     fun submitList(list: List<ItemType>)
+    fun getItems(): List<ItemType>
     fun getItemCount(): Int
     fun getItemAt(position: Int): ItemType
     fun getPositionOfItem(item: ItemType): Int
